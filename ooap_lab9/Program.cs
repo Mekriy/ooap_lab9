a5e28e1a9eebf12efe6b2fb0a268e21c774b7784
Andrii Pasternak <a.pasternak@nltu.lviv.ua>
2024-04-29 21:41:05 +0300

    initial commit

---
+++ ooap_lab9/Program.cs
@@ -0,0 +1,53 @@
+﻿public enum State
+{
+    NewOrder,
+    Shipped,
+    Invoiced
+}
+public class Order
+{
+    public State State { get; private set; }
+
+    public Order()
+    {
+        State = State.NewOrder; 
+    }
+    public void ProcessOrder()
+    {
+        switch (State)
+        {
+            case State.NewOrder:
+                ShipOrder();
+                break;
+            case State.Shipped:
+                InvoiceOrder(); 
+                break;
+            default:
+                Console.WriteLine("Недопустимий перехід стану.");
+                break;
+        }
+    }
+    private void ShipOrder()
+    {
+        Console.WriteLine("Замовлення відправлено.");
+        State = State.Shipped;
+    }
+    private void InvoiceOrder()
+    {
+        Console.WriteLine("Замовлення оплачено.");
+        State = State.Invoiced;
+    }
+}
+
+internal static class Program
+{
+    private static void Main()
+    {
+        Order order = new Order();
+        Console.WriteLine("Початковий стан замовлення: " + order.State);
+        order.ProcessOrder();
+        Console.WriteLine("Поточний стан замовлення: " + order.State);
+        order.ProcessOrder(); 
+        Console.WriteLine("Поточний стан замовлення: " + order.State);
+    }
+}
